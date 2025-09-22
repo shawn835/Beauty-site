97f8657e235011a4e95e49aa36eb2b8cf6ce520f
Shawn <nahashonm851@gmail.com>
2025-09-22 18:30:41 +0300

    profile

--- src/router/router.js
+++ src/router/router.js
@@ -38,7 +38,7 @@ const router = createRouter({
     {
       path: "/profile",
       name: "profile",
-      component: () => import("@/views/profileView.vue"),
+      component: () => import("@/views/ProfileView.vue"),
       meta: { requiresAuth: true },
     },
     {
